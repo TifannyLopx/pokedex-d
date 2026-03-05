8efa54aa58c82ac269d800d8eca496620dd26aa1
EstefaniaLopez <lopxtifanny@gmail.com>
2026-03-04 19:58:59 -0600

    Show pokemons on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const [results,setResults] = useState<any[]>([]);
   useEffect(() => {
     console.log("Entre en pantalla");
     getPokemons();
@@ -10,11 +11,17 @@ export default function Index() {
     const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
     const response = await fetch(URL);
     console.log(response);
-    const data = await response.json;
+    const data = await response.json();
+    setResults(data.results);
   };
   return (
     <View>
-      <Text>Tifanny</Text>
+      <Text>{results[250]?.name}</Text>
+      {
+        results.map((item)=>{
+          return <Text key={item.name}>{item.name}</Text>
+        })
+      }
     </View>
   );
 }
